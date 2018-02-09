88357ea5ce170b4bd8b2439a855fe142821bc7ac
3Pair <36264650+3Pair@users.noreply.github.com>
2018-02-09 03:06:40 -0500

    Update RunMeInPython.py

--- RunMeInPython.py
+++ RunMeInPython.py
@@ -4,32 +4,24 @@ def esPrimo(n):
         return False
     else:
         arr = arange(2,int(n**(1/2)+1))
-        c = 0
         for i in arr:
             if n%i == 0:
-                c = c + 1
-        if c == 0:
-            return True
-        else:
-            return False
+                return False
+        return True
 def esPrimoGaussiano(a,b):
-    if (a == 0):
-        if (abs(b)%4 == 3):
+    n2 = a**2 + b**2
+    if (a*b == 0):
+        n = int(n2**(1/2))
+        if (n%4 == 3):
             return True
-    if (b == 0):
-        if (abs(a)%4 == 3):
-            return True
-    if (a!=0 and b!=0):
-        n = (a**2) + (b**2)
-        if esPrimo(n):
+    else:
+        if esPrimo(n2):
             return True
     return False
 r = 5
 arr = arange(-r,r+1)
 for a in arr:
     for b in arr:
-        p = a**2 + b**2
-        if (p <= r**2):
-            if esPrimoGaussiano(a,b):
-                x = complex(a,b)
-                print(x)
+        if esPrimoGaussiano(a,b):
+            x = complex(a,b)
+            print(x)
